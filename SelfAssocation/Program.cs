cf955903588ceb12e4dfbae22593317718a8ccbc
ArmenHovhannisyan1998 <48031430+ArmenHovhannisyan1998@users.noreply.github.com>
2019-03-16 21:14:36 +0400

    Add project files.

---
+++ SelfAssocation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfAssocation
+{
+    class Program
+    {
+        public void Method()
+        {
+            Console.WriteLine("Hello World !");
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
